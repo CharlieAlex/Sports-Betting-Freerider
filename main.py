9e860919696bc70d874a830d36e1b16464f9bb57
CharlieAlex <42445052+CharlieAlex@users.noreply.github.com>
2023-11-24 13:41:49 +0800

    complete version1

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from sport_crawler import Leaderboard, Rank_user
+from data_process import Output_maker
 from config import alliance_dict
 import pandas as pd
 from tqdm import trange
@@ -7,17 +8,19 @@ import random
 from datetime import date
 
 target = 'NBA'
-during = 'lastmonth'
+during = 'thismonth'
 today = date.today().strftime("%Y%m%d")
 rawdata_path = '/Users/alexlo/Desktop/Project/Sport_Lottery/rawdata'
+workdata_path = '/Users/alexlo/Desktop/Project/Sport_Lottery/workdata'
 
 if __name__ == '__main__':
     #獲得排行榜數據
-    rank_list = Leaderboard(during, alliance_dict[target])
+    rank_list = Leaderboard(alliance_dict[target], during)
     leaderboard = rank_list.dataframe
     leaderboard.to_csv(f'{rawdata_path}/leaderboard_{target}_{today}.csv', index=False)
+    leaderboard = leaderboard[leaderboard['mode'] == '國際盤賽事']
 
-    #獲得排行榜上每一個人的預測數據
+    #獲得國際盤排行榜上每一個人的預測數據
     all_prediction = pd.DataFrame()
     crawl_count = 30 if leaderboard.shape[0] > 30 else leaderboard.shape[0]
     collected_count = 0
@@ -28,7 +31,12 @@ if __name__ == '__main__':
             collected_count = collected_count + 1 if user.prediction.shape[0] > 0 else collected_count
         except Exception as e:
             print(e)
-        if collected_count >= 10:
+        if collected_count >= 15:
             break
-        time.sleep(random.random()*5)
+        time.sleep(random.random()*15)
     all_prediction.to_csv(f'{rawdata_path}/prediction_{target}_{today}.csv', index=False)
+
+    #統計結果
+    output = Output_maker(leaderboard, all_prediction)
+    output.mainpush_summary.to_csv(f'{workdata_path}/mainpush_{target}_{today}.csv', index=False)
+    output.total_summary.to_csv(f'{workdata_path}/total_{target}_{today}.csv', index=False)
