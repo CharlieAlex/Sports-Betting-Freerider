d1d552128a298caf4d904f46670c772b9d1e306a
CharlieAlex <42445052+CharlieAlex@users.noreply.github.com>
2023-11-27 08:31:32 +0800

    Add crawl_num parameter

--- linebot_main.py
+++ linebot_main.py
@@ -7,10 +7,10 @@ from linebot.models import (
     TextMessage, StickerSendMessage,
     )
 
-def linebot_main(target, during):
+def linebot_main(target, during, target_num):
     print('開始爬蟲!')
     today = date.today().strftime("%Y%m%d")
-    leaderboard, prediction = main(target, during)
+    leaderboard, prediction = main(target, during, target_num)
     print('爬蟲完畢')
     output = Output_maker(leaderboard, prediction)
     data = {
@@ -31,10 +31,10 @@ def echo_text(event):
     try:
         if rm_list[0] == 'Start':
             if (rm_list[1] in alliance_dict.keys()) & (rm_list[2] in during_list):
-                linebot_main(rm_list[1], rm_list[2])
+                linebot_main(rm_list[1], rm_list[2], rm_list[3])
                 sent_message = TextSendMessage(text='已完成爬蟲，請前往收信')
             else:
-                sent_message = TextSendMessage(text='指令有誤，請重新輸入以下格式: Start NBA thismonth')
+                sent_message = TextSendMessage(text='指令有誤，請重新輸入以下格式: Start NBA thismonth 15')
         else:
             sent_message = StickerSendMessage(package_id='6359', sticker_id='11069851')
     except Exception as e:
--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import time
 import random
 from datetime import date
 
-def main(target, during):
+def main(target, during, target_num):
     #獲得排行榜數據
     rank_list = Leaderboard(alliance_dict[target], during)
     leaderboard = rank_list.dataframe
@@ -18,18 +18,18 @@ def main(target, during):
 
     #獲得國際盤排行榜上每一個人的預測數據
     all_prediction = pd.DataFrame()
-    crawl_count = 30 if leaderboard.shape[0] > 30 else leaderboard.shape[0]
+    crawl_num = 30 if leaderboard.shape[0] > 30 else leaderboard.shape[0]
     collected_count = 0
-    for i in trange(crawl_count):
+    for i in trange(crawl_num):
         try:
             user = Rank_user(leaderboard.iloc[i])
             all_prediction = pd.concat([all_prediction, user.prediction])
             collected_count = collected_count + 1 if user.prediction.shape[0] > 0 else collected_count
         except Exception as e:
             print(e)
-        if collected_count >= 15:
+        if collected_count >= target_num:
             break
-        time.sleep(random.random()*15)
+        time.sleep(random.random()*5)
 
     return leaderboard, all_prediction
 
@@ -42,7 +42,7 @@ if __name__ == '__main__':
     target = 'NBA'
     during = 'thismonth'
     today = date.today().strftime("%Y%m%d")
-    leaderboard, prediction = main(target, during)
+    leaderboard, prediction = main(target, during, 15)
     leaderboard.to_csv(f'{rawdata_path}/leaderboard_{target}_{today}.csv', index=False)
     prediction.to_csv(f'{rawdata_path}/prediction_{target}_{today}.csv', index=False)
     print('爬蟲完畢')
