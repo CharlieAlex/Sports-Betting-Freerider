46230dbeba45e3d74ca0a095acee33f4a39e238e
CharlieAlex <42445052+CharlieAlex@users.noreply.github.com>
2024-06-04 10:15:31 +0800

    Add query bigquery function

---
+++ gcp/query_bq_table.py
@@ -0,0 +1,31 @@
+from google.cloud import bigquery
+from google.cloud.bigquery.client import Client
+import pandas as pd
+import streamlit as st
+
+
+@st.cache_resource(show_spinner=True)
+def load_data(
+    _client: Client,
+    myquery: str,
+) -> pd.DataFrame:
+    query_job = _client.query(myquery)
+    rows = query_job.result()
+    df = rows.to_dataframe()
+    df["date"] = pd.to_datetime(df["date"])
+    print("load data")
+    return df
+
+
+if "__name__" == "__main__":
+    bq_key = "/Users/alexlo/Desktop/Project/Sport_Lottery/big-query.json"
+
+    client = bigquery.Client.from_service_account_json(json_credentials_path=bq_key)
+
+    query_today_totalresult = """
+        SELECT game, prediction, result
+        FROM `sport-lottery-database.playsports.total_result`
+        WHERE date = '2024-05-28'
+    """
+
+    load_data(client, query_today_totalresult)
